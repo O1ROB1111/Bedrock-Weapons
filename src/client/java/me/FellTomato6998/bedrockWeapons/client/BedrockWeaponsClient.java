36d52302298d7c4f9731171743ba47346a43f366
FellTomato6998 <oliverback.robaczewskiup@gmail.com>
2026-02-16 11:48:31 +0000

    Initial commit

---
+++ src/client/java/me/FellTomato6998/bedrockWeapons/client/BedrockWeaponsClient.java
@@ -0,0 +1,10 @@
+package me.FellTomato6998.bedrockWeapons.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BedrockWeaponsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/me/FellTomato6998/bedrockWeapons/BedrockWeapons.java
@@ -0,0 +1,10 @@
+package me.FellTomato6998.bedrockWeapons;
+
+import net.fabricmc.api.ModInitializer;
+
+public class BedrockWeapons implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
